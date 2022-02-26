c695ca97a7f6c754f3782e1c38b29cb07bd1bb8a
ecrseer <juustino@live.com>
2022-02-26 13:55:04 -0300

    database applicationok

--- app/src/main/java/br/infnet/dr3_gabriel_justino_tp3/domain/Models.kt
+++ app/src/main/java/br/infnet/dr3_gabriel_justino_tp3/domain/Models.kt
@@ -2,6 +2,7 @@ package br.infnet.dr3_gabriel_justino_tp3.domain
 
 import androidx.annotation.WorkerThread
 import androidx.room.Entity
+import androidx.room.Insert
 import androidx.room.PrimaryKey
 import androidx.room.Query
 
@@ -16,12 +17,21 @@ data class EvaluatorSession(
 interface EvaluatorSessionDAO{
     @Query("SELECT * FROM evaluatorsession")
     fun getAll():List<EvaluatorSession>
+    @Insert
+    fun insert(evaluatorSession: EvaluatorSession):Long
 }
 class EvaluatorRepository(private val evaluatorSessionDAO: EvaluatorSessionDAO){
     @Suppress("RedundantSuspendModifier")
     @WorkerThread
     suspend fun getAllSessions (): List<EvaluatorSession> {
         return evaluatorSessionDAO.getAll()
+
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun addNewSession (evaluatorSession: EvaluatorSession):Long{
+        return evaluatorSessionDAO.insert(evaluatorSession)
 
     }
 
